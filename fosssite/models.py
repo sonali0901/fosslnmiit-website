c13f88d2e9fbd547a7960894cd18715c6bfb0b23
sonali0901 <sonaligpt0@gmail.com>
2016-07-20 19:34:29 +0530

    authentication

--- fosssite/models.py
+++ fosssite/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class User(models.Model):
+	firstname=models.CharField(max_length=200)
+	lastname=models.CharField(max_length=200)
+	username=models.CharField(max_length=200)
+	email=models.ForeignKey('auth.User')
+	password=models.CharField(max_length=200)
--- fosssite/views.py
+++ fosssite/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404
-from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404,render_to_response,redirect
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.core.context_processors import csrf
 
 # Create your views here.
 
@@ -9,3 +10,28 @@ def home(request):
 	return render(request, 'fosssite/home.html')
 def signup(request):
 	return render(request,'fosssite/signup.html')
+def login(request):
+	c={}
+	c.update(csrf(request))
+	return render_to_response('fosssite/login.html',c)
+
+def auth_view(request):
+	username=request.POST.get('username', '')
+	password=request.POST.get('password', '')
+	user=auth.authenticate(username=username,password=password)
+
+	if user is not None:
+		auth.login(request,user)
+		return HttpResponseRedirect('/loggedin')#url in brackets
+	else:
+		return HttpResponseRedirect('/invalid')
+
+def loggedin(request):
+	return render_to_response('fosssite/loggedin.html',{'fullname':request.user.username})
+
+def logout(request):
+	auth.logout(request)
+	return render_to_response('fosssite/logout.html')
+
+def invalid_login(request):
+	return render_to_response('fosssite/invalid_login.html')
