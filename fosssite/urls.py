f7db9798c0b63bea1b0f1ab0bc4e6c936d2e2529
sonali0901 <sonaligpt0@gmail.com>
2016-07-26 10:20:47 +0530

    signup

--- fosssite/urls.py
+++ fosssite/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = [
 	url(r'^$', views.home, name='home'),
-	url(r'^signup/$', views.signup, name='signup'),
+	url(r'^signup/$', views.UserFormView, name='UserFormView'),
 	url(r'^login/$',views.login,name='login'),
 	url(r'^auth/$',views.auth_view,name='auth_view'),
 	url(r'^loggedin/$',views.loggedin,name='loggedin'),
--- fosssite/views.py
+++ fosssite/views.py
@@ -1,19 +1,46 @@
+from django.views.generic import View
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404,render_to_response,redirect
 from django.http import HttpResponseRedirect
 from django.contrib import auth
+from django.contrib.auth import authenticate,login
 from django.core.context_processors import csrf
+from .forms import UserForm
 
 # Create your views here.
 
 def home(request):
 	return render(request, 'fosssite/home.html')
-def signup(request):
-	return render(request,'fosssite/signup.html')
 def login(request):
 	c={}
 	c.update(csrf(request))
 	return render_to_response('fosssite/login.html',c)
+	
+def UserFormView(request):
+	form_class=UserForm
+	template_name='fosssite/signup.html'
+
+	if request.method=='GET':
+		form=form_class(None)
+		return render(request,template_name,{'form':form})
+
+
+		#validate by forms of django
+	if request.method=='POST':
+		form=form_class(request.POST)
+
+		if form.is_valid():
+			# not saving to database only creating object
+			user=form.save(commit=False)
+			#normalized data
+			username=form.cleaned_data['username']
+			password=form.cleaned_data['password']
+			#not as plain data
+			user.set_password(password)
+			user.save() #saved to database
+			return render_to_response('fosssite/home.html',user)
+		else:
+			return render(request,template_name,{'form':form})
 
 def auth_view(request):
 	username=request.POST.get('username', '')
