01abf709266bcd5de469b7d77343206fe4f4d4e6
HItesh Garg <garg95hitesh@gmail.com>
2016-08-05 00:48:13 +0530

    login,signup,logout working properly

--- fosssite/urls.py
+++ fosssite/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
 from . import views
-from django.conf.urls import patterns, url
+from django.conf.urls import  url
 
-
+app_name = 'fosssite'
 urlpatterns = [
 	url(r'^$', views.home, name='home'),
 	url(r'^signup/$', views.UserFormView, name='UserFormView'),
-	url(r'^login/$',views.login,name='login'),
-	url(r'^auth/$',views.auth_view,name='auth_view'),
+	url(r'^login/$',views.login_user,name='login_user'),
+	#url(r'^auth/$',views.auth_view,name='auth_view'),
 	url(r'^profileuser/$',views.profileuser,name='profileuser'),
 	url(r'^logout/$',views.logout,name='logout'),
 	url(r'^edit_user_profile/$',views.edit_user_profile,name='edit_user_profile'),
--- fosssite/views.py
+++ fosssite/views.py
@@ -1,53 +1,56 @@
 from django.views.generic import View
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404,render_to_response,redirect
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib import auth
-from django.contrib.auth import authenticate,login
-from django.core.context_processors import csrf
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+#from django.core.context_processors import csrf
+#from django.views.decorators import csrf
 from passlib.hash import pbkdf2_sha256
 from .forms import UserForm
 
 # Create your views here.
 
 def home(request):
+	if request.user.is_authenticated():
+		return redirect('fosssite:profileuser')
 	return render(request, 'fosssite/home.html')
 
-def login(request):
-	c={}
-	c.update(csrf(request))
-	return render_to_response('fosssite/login.html',c)
+def login_user(request):
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username,password=password)
+		if user is not None:
+			auth.login(request, user)
+			#return render(request, 'fosssite/profileuser.html', {'username': user.username})
+			return redirect('fosssite:profileuser')
+		else:
+			return render(request, 'fosssite/login.html', {'error_message': 'Invalid login'})
+	return render(request,'fosssite/login.html')
+
 
 def UserFormView(request):
-	form_class=UserForm
-	template_name='fosssite/signup.html'
-
-	if request.method=='GET':
-		form=form_class(None)
-		return render(request,template_name,{'form':form})
-
-
-		#validate by forms of django
-	if request.method=='POST':
-		form=form_class(request.POST)
-
-		if form.is_valid():
-			# not saving to database only creating object
-			user=form.save(commit=False)
-			#normalized data
-			username=form.cleaned_data['username']
-			text_password=form.cleaned_data['password']
-			#not as plain data
-			password=pbkdf2_sha256.encrypt(text_password,rounds=12000,salt_size=32)
-			user.save() #saved to database
-
-			user=auth.authenticate(username=username,password=password)
-			if user is not None:
-				auth.login(request,user)
-				return HttpResponseRedirect('/profileuser')#url in brackets
-
-			return render(request,template_name,{'form':form})
-
+	form=UserForm(request.POST or None)
+	if form.is_valid():
+		# not saving to database only creating object
+		user=form.save(commit=False)
+		#normalized data
+		user.first_name = form.data['fname']
+		user.last_name = form.data['lname']
+		username=form.cleaned_data['username']
+		password=form.cleaned_data['password']
+		#not as plain data
+		#password=pbkdf2_sha256.encrypt(text_password,rounds=12000,salt_size=32)
+		user.set_password(password)
+		user.save() #saved to database
+		user = auth.authenticate(username=username,password=password)
+		if user is not None:
+			auth.login(request, user)
+			return redirect('fosssite:profileuser')
+	return render(request,'fosssite/signup.html',{'form':form})
+"""
 def auth_view(request):
 	username=request.POST.get('username', '')
 	password=request.POST.get('password', '')
@@ -58,10 +61,10 @@ def auth_view(request):
 		return HttpResponseRedirect('/profileuser')#url in brackets
 	else:
 		return HttpResponseRedirect('/invalid')
-
+"""
 def profileuser(request):
 	#url = request.user.profile.url
-	return render_to_response('fosssite/profileuser.html',{'username':request.user.username})
+	return render(request,'fosssite/profileuser.html',{'username':request.user.username})
 
 def logout(request):
 	auth.logout(request)
@@ -69,9 +72,10 @@ def logout(request):
 
 def edit_user_profile(request):
 	return  render_to_response('fosssite/edituser.html')
-
+"""
 def invalid_login(request):
 	return render_to_response('fosssite/invalid_login.html')
 
 def view_profile(request):
     url = request.user.profile.url
+"""
