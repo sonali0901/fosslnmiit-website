da4d8884152bf47ba3129082049049eae7264773
HItesh Garg <garg95hitesh@gmail.com>
2016-08-28 00:54:04 +0530

    add contributions implemented

--- fosssite/admin.py
+++ fosssite/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from fosssite.models import UserProfile
+from fosssite.models import UserProfile, Contributions, Speakers
 
 admin.site.register(UserProfile)
+admin.site.register(Contributions)
+admin.site.register(Speakers)
